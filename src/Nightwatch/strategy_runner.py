a8ecb1520af820accdc1bc20817ea91792ca1e06
Julien Haefelin <julien.haefelin@sbb.ch>
2026-03-19 07:56:39 +0100

    feat: implement log for signal event and change log levels

--- src/Nightwatch/strategy_runner.py
+++ src/Nightwatch/strategy_runner.py
@@ -30,15 +30,25 @@ class StrategyRunner:
         first_tick = self._buffer.get_first_tick(tick.symbol)
         self._buffer.add_tick(tick)
         if not first_tick:
-            LOGGER.debug("Received first tick for symbol %s: %s", tick.symbol, tick)
+            LOGGER.warning("Received first tick for symbol %s: %s", tick.symbol, tick)
             return None
         if self._last_signal_time is not None and tick.timestamp - self._last_signal_time <= self._cooldown:
-            LOGGER.debug("Tick for symbol %s received during cooldown period: %s", tick.symbol, tick)
+            LOGGER.warning("Tick for symbol %s received during cooldown period: %s", tick.symbol, tick)
             return None
 
         signal = self._strategy.on_tick(symbol=tick.symbol, window=self._buffer.get_ticks(tick.symbol))
         if signal:
-            LOGGER.debug("Emitting signal: %s for symbol: %s", signal, tick.symbol)
+            log = {
+                "event": "signal",
+                "signal_id": signal.uid,
+                "symbol": tick.symbol,
+                "side": signal.side,
+                "strategy": signal.strategy,
+                "delta_pct": signal.rationale.get("delta_pct", None),
+                "window_sec": signal.rationale.get("window_sec", None),
+                "threshold_pct": signal.rationale.get("threshold_pct", None),
+            }
+            LOGGER.debug(log)
             self._last_signal_time = tick.timestamp
             if self._metric:
                 self._metric.signals_total.labels(symbol=tick.symbol, side=signal.side).inc()
